cb58d51e4363812b4acf7c1ab6b315d67d133438
Rijil Jayaprakasan <rijiljayaprakash@gmail.com>
2023-05-08 19:40:57 +0530

    Initial commit

---
+++ src/main/java/com/inow/csp/ConsumerServicePortalApplication.java
@@ -0,0 +1,13 @@
+package com.inow.csp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerServicePortalApplication {
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ConsumerServicePortalApplication.class, args);
+	}
+
+}
