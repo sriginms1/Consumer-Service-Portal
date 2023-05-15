698deca5e935edf83d41bbea5318bf12be1454d4
Rijil Jayaprakasan <rijiljayaprakash@gmail.com>
2023-05-15 18:16:16 +0530

    customer register start service updates

--- src/main/java/com/inow/csp/service/impl/CustomerRegistrationServiceImpl.java
+++ src/main/java/com/inow/csp/service/impl/CustomerRegistrationServiceImpl.java
@@ -2,10 +2,15 @@ package com.inow.csp.service.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 
 import com.inow.csp.config.Constants;
 import com.inow.csp.output.customerRegistration.CustomerRegisterStartBean;
 import com.inow.csp.service.ICustomerRegistrationService;
+
+
 
 @Service("customerRegistrationServiceImpl")
 public class CustomerRegistrationServiceImpl implements ICustomerRegistrationService{
@@ -19,12 +24,16 @@ public class CustomerRegistrationServiceImpl implements ICustomerRegistrationSer
 	@Override
 	public CustomerRegisterStartBean fetchCustomerRegisterStartInfo(String policyNumber) {
 		 String url = Constants.SERVER_HOST + Constants.CUSTOMER_REG_START_URI_PART;
+		 String jsonBody = "{\"policyNumber\":\"" + policyNumber + "\"}";
 		CustomerRegisterStartBean customerRegisterStart = new CustomerRegisterStartBean();
-		return webClient.get()
+		return webClient.post()
                 .uri(url)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(BodyInserters.fromValue(jsonBody))
                 .retrieve()
                 .bodyToMono(CustomerRegisterStartBean.class)
                 .block();
+    }
 		 
 	}
 	
